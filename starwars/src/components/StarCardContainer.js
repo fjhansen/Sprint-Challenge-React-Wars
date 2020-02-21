87a1827a4ee6cbf9e7484a898568b059378a9470
Frances Hansen <francesjuniperhansen@gmail.com>
2020-02-21 09:15:14 -0800

    styling

--- starwars/src/components/StarCardContainer.js
+++ starwars/src/components/StarCardContainer.js
@@ -1,10 +1,22 @@
 import React from "react"
+import { StarCardList, StarCardTitle } from "../config/theme";
+
 
 const StarCardContainer = props => {
   console.log(props)
   return(
-    <div>
-      <h3>{props.character.name}</h3>
+    <div className="StarCardContainer">
+     <StarCardTitle>{props.character.name}</StarCardTitle>
+  <StarCardList>
+  <li>Gender: {props.character.gender}</li>
+  <li>Birth Year: {props.character.birth_year}</li>
+  <li>Height: {props.character.height}</li>
+  <li>Mass: {props.character.mass}</li>
+  <li>Hair Color: {props.character.hair_color}</li>
+  <li>Eye Color: {props.character.eye_color}</li>
+  <li>Skin Color: {props.character.skin_color}</li>
+  </StarCardList>
+     
     </div>
   )
 }
--- starwars/src/config/theme.js
+++ starwars/src/config/theme.js
@@ -5,4 +5,25 @@ width: 69%;
 display: flex;
 flex-wrap: wrap;
 margin: auto;
+
+`
+
+export const StarCardList = styled.ul `
+background: white;
+margin: 3%;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+line-height: 1.5;
+`
+
+export const StarCardTitle = styled.h3 `
+background-color: #fff;
+display: flex;
+justify-content: center;
+align-items: center;
+
+margin: 3%;
+
 `
